deb8a2822411c83d84754ae74307cb5c5a61b8a0
mapalmieri <maupalm88@gmail.com>
2018-10-05 11:01:12 +0200

    added the multi-UAV demos

--- examples/demos/Multi-UAVs/GUI/js/index.js
+++ examples/demos/Multi-UAVs/GUI/js/index.js
@@ -16,6 +16,8 @@ require([
     system.green_drone = d3.select("#green_drone");
     system.red_drone = d3.select("#red_drone");
     system.pink_drone = d3.select("#pink_drone");
+    
+    system.collision_detect = false;
 
     function onMessageReceived(err, res) {
         var state = PVSioStateParser.parse(res);
@@ -28,11 +30,26 @@ require([
         };
 
         // don't use the Navigator widget, just move the drones with the following command.
-        system.blue_drone.style("left", 100+pos.blue_drone.x*100 + "px").style("top", 400-pos.blue_drone.y*100 + "px");
-        system.red_drone.style("left", 100+pos.red_drone.x*100 + "px").style("top", 400-pos.red_drone.y*100 + "px");
-        system.yellow_drone.style("left", 100+pos.yellow_drone.x*100 + "px").style("top", 400-pos.yellow_drone.y*100 + "px");
-        system.green_drone.style("left", 100+pos.green_drone.x*100 + "px").style("top", 400-pos.green_drone.y*100 + "px");
-        system.pink_drone.style("left", 100+pos.pink_drone.x*100 + "px").style("top", 400-pos.pink_drone.y*100 + "px");
+        			
+			
+			system.blue_drone.style("left", 100+pos.blue_drone.x*100 + "px").style("top", 400-pos.blue_drone.y*100 + "px").style("transition-duration",250+"ms");
+			system.red_drone.style("left", 100+pos.red_drone.x*100 + "px").style("top", 400-pos.red_drone.y*100 + "px").style("transition-duration",250+"ms");
+			system.yellow_drone.style("left", 100+pos.yellow_drone.x*100 + "px").style("top", 400-pos.yellow_drone.y*100 + "px").style("transition-duration",250+"ms");
+			system.green_drone.style("left", 100+pos.green_drone.x*100 + "px").style("top", 400-pos.green_drone.y*100 + "px").style("transition-duration",250+"ms");
+			system.pink_drone.style("left", 100+pos.pink_drone.x*100 + "px").style("top", 400-pos.pink_drone.y*100 + "px").style("transition-duration",250+"ms");
+		
+		//assume drone start far enough
+		if (system.collision_detect){
+			system.green_drone.style("transform", "rotate(130deg)").style("top", 600+"px").style("transition-duration",2000+"ms");
+			system.red_drone.style("transform", "rotate(210deg)").style("top", 600+"px").style("transition-duration",2000+"ms");
+			stop_tick();
+		}
+		// 3 drones moving, only the middle one (green) can collide
+		else if(((Math.abs(pos.green_drone.x-pos.yellow_drone.x) < 0.7) || (Math.abs(pos.green_drone.x-pos.red_drone.x) < 0.7)) && (Math.abs(pos.green_drone.y-pos.yellow_drone.y) < 0.5)){ 
+		system.collision_detect=true;
+		}
+		
+		console.log(pos);
     }
 
     // web socket client
